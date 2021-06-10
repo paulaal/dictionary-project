c5ff415a4a0cae9d28f054936e1a110b8c6c5356
palcorta <78558036+paulaal@users.noreply.github.com>
2021-06-10 13:19:28 +0200

    Included phonetics

--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
 
 export default function Results(props) {
 	if (props.data) {
 		return (
 			<div className="Results">
 				<h2 className="Results-word">{props.data.word}</h2>
+				{props.data.phonetics.map(function (phonetics, index) {
+					return (
+						<div key={index}>
+							<Phonetics phonetics={phonetics} />
+						</div>
+					);
+				})}
 				{props.data.meanings.map(function (meaning, index) {
 					return (
 						<div key={index}>
